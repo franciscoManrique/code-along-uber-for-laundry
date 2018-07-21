c830f1e113b73505a1186793b6e7a764fc19b18a
fran <fmls1989@gmail.com>
2018-07-21 13:24:43 +0200

    fix login redirect

--- starter_code/controllers/sessions.controller.js
+++ starter_code/controllers/sessions.controller.js
@@ -1,3 +1,4 @@
+
 module.exports.create = () =>{
     res.render('/auth/signup');
 };
--- starter_code/controllers/users.controller.js
+++ starter_code/controllers/users.controller.js
@@ -1,9 +1,11 @@
-const User = require('../models/user');
 
+const User = require('../models/user');
+const passport = require('passport');
 
 module.exports.create = (req, res, next) =>{
-    res.render('users/create');
+    res.render('users/usersCreate');
 };
+
 module.exports.doCreate = (req, res, next) =>{
     if (!req.body.email || !req.body.password) {
         res.render('users/create', {erros: {email:'email or password is empty'}});
@@ -11,13 +13,13 @@ module.exports.doCreate = (req, res, next) =>{
         User.findOne({email: req.body.email})
         .then(user =>{
             if (user) {
-                res.render('users/create', {erros: {email:'email is already chosen'}});
+                res.render('users/usersCreate', {erros: {email:'email is already chosen'}});
                 
             } else{
                  const newUser = new User(req.body);
                  return newUser.save()
                  .then((user) => {
-                    res.render('sessions/sessions.create')
+                    res.render('sessions/sessionsCreate')
 
 
                  })
