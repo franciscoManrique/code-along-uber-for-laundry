10885ae299f9ab2ed9751ce56b6bc6f39285cab0
fran <fmls1989@gmail.com>
2018-07-21 13:09:28 +0200

    Fourth commmit

--- starter_code/controllers/users.controller.js
+++ starter_code/controllers/users.controller.js
@@ -6,15 +6,22 @@ module.exports.create = (req, res, next) =>{
 };
 module.exports.doCreate = (req, res, next) =>{
     if (!req.body.email || !req.body.password) {
-        res.render('users/create', {erros: {email:'email is already chosen'}});
+        res.render('users/create', {erros: {email:'email or password is empty'}});
     } else{
         User.findOne({email: req.body.email})
         .then(user =>{
             if (user) {
-                console.log(user);
+                res.render('users/create', {erros: {email:'email is already chosen'}});
                 
             } else{
-                // const new
+                 const newUser = new User(req.body);
+                 return newUser.save()
+                 .then((user) => {
+                    res.render('sessions/sessions.create')
+
+
+                 })
+                 
             }
         })
         .catch(error =>{
